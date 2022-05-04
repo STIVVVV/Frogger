c3f86328db12dbb0a775ee22211ed57740e4c8a9
STIVVVV <stevencanfield2000@gmail.com>
2022-05-03 23:27:27 -0400

    Added basic frog movement.
    just need to fix the trail it leaves behind it.

--- app.js
+++ app.js
@@ -3,3 +3,32 @@ const resultDisplay = document.querySelector('#result')
 const StartPauseButton = document.querySelector('#start-pause-button')
 const squares = document.querySelectorAll('.grid div')
 
+let currentIndex = 76
+const width = 9
+
+function moveFrog(e)
+{
+    switch(e.key)
+    {
+        case 'ArrowLeft':
+            console.log('move Left')
+            currentIndex -= 1
+            break
+        case 'ArrowRight':
+            console.log('move Right')
+            currentIndex += 1
+            break
+        case 'ArrowUp':
+            console.log('move Up')
+            currentIndex -= width
+            break
+        case 'ArrowDown':
+            console.log('move Down')
+            currentIndex += width
+            break       
+    }
+
+    squares[currentIndex].classList.add('frog')
+}
+
+document.addEventListener('keyup', moveFrog)
